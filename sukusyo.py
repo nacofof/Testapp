b001a11492935ae1e4c651adc5672932605db342
nacofof <aiei.numasu@gmail.com>
2020-12-08 16:05:21 +0900

    clipboard(FHD)->clipboard(HD)

--- sukusyo.py
+++ sukusyo.py
@@ -1,15 +1,27 @@
 import tkinter as tk
-# import win32clipboard as winc
+import win32clipboard as w32c
 from PIL import ImageGrab
 from PIL import Image
+import io
 
 # ボタンを押したときの処理
 def grabimage():
     # クリップボードから画像を取得する
     im = ImageGrab.grabclipboard()
+    # 画像を取得できなければ終了する
+    if im is None:
+        labelkanshi['text'] = 'None'
+        return
+    # 画像を縮小する
     small_image = im.resize((1280, 720))
-    im.save('somefile.png', 'PNG')
-    small_image.save('somefile2.png', 'PNG')
+    output = io.BytesIO()
+    small_image.convert("RGB").save(output,"BMP")
+    data = output.getvalue()[14:]
+    output.close()
+    w32c.OpenClipboard()
+    w32c.SetClipboardData(w32c.CF_DIB, data)
+    w32c.CloseClipboard()
+    labelkanshi['text']='Success!'
 
 # 画面の表示
 win = tk.Tk()
@@ -17,10 +29,12 @@ win.title("スクショ圧縮")
 win.geometry("500x300")
 
 # 部品を作成
-labelkanshi = tk.Label(win, text="NONE")
+labelsetsumei = tk.Label(win, text='クリップボードの画像を縮小します')
+labelsetsumei.pack()
+labelkanshi = tk.Label(win, text="---")
 labelkanshi.pack()
 
-button = tk.Button(win, text='PNG生成', command = grabimage )
+button = tk.Button(win, text='縮小', command = grabimage )
 button.pack()
 # ウィンドウを動かす
 win.mainloop()
